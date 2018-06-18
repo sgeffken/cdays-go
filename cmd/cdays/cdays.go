5e3bb80b96668ae6b5ec70463185d19389ba4703
Soeren Geffken <sgeffken@math.uni-bremen.de>
2018-06-18 15:01:42 +0200

    Added logger

---
+++ cmd/cdays/cdays.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func main() {
+	log.Print("The application is starting...")
+	
+	http.HandleFunc("/", rootHandler())
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func rootHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!"))			
+	}
+}
